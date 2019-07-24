ceac2162edbc9c178e099082f9db6ddd3dc89a85
Mikhail Hathey <mikhail.hathey@gmail.com>
2019-07-24 23:09:53 +0200

    Add Tests for Employee Repository, Factory and Service

---
+++ src/test/java/ac/za/cput/factory/user/EmployeeFactoryTest.java
@@ -0,0 +1,17 @@
+package ac.za.cput.factory.user;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EmployeeFactoryTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void buildEmployee() {
+    }
+}
---
+++ src/test/java/ac/za/cput/service/EmployeeServiceTest.java
@@ -0,0 +1,12 @@
+package ac.za.cput.service;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class EmployeeServiceTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+}
