e023310c8da92b5da73a5147d897c2fd0aad858f
Mikhail Hathey <mikhail.hathey@gmail.com>
2019-08-10 19:23:18 +0200

    Add Controller Tests

---
+++ src/test/java/com/hathey/controller/demography/RaceControllerTest.java
@@ -0,0 +1,34 @@
+package com.hathey.controller.demography;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RaceControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void create() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void read() {
+    }
+}
